d766aa6f76c79b5532a57d607f0c3906b060173d
Sayantan Mandal <sayantanman508@gmail.com>
2026-08-15 22:46:32 +0530

    feat: update version string to v2.0 (#7)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from display import show_all_tasks, show_task_count
 from storage import load_tasks, save_tasks, delete_task
 
 def main():
-    print("Task Manager v1.0")
+    print("Task Manager v2.0 -- by Sayan")
     print("------------------")
     tasks = load_tasks()
 
